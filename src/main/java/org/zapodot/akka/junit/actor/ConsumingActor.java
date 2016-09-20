ce932502821e5daa0d5b0783c4c45d41f9b9e83e
Sondre Eikanger Kvalø <zapodot@gmail.com>
2016-09-20 22:39:20 +0200

    Tweak comments

--- src/main/java/org/zapodot/akka/junit/actor/ConsumingActor.java
+++ src/main/java/org/zapodot/akka/junit/actor/ConsumingActor.java
@@ -7,6 +7,9 @@ import akka.event.LoggingAdapter;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * A simple consuming actor that stores all messages that was received
+ */
 public class ConsumingActor extends UntypedActor {
 
     public List<Object> messagesReceived = new LinkedList<>();
